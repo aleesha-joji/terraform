f49394dbb4f97076c11fa05dc1aa2ae3d7fc7eea
Aleesha Joji <78786418+aleesha-joji@users.noreply.github.com>
2021-06-23 03:32:46 +0530

    Update ReactApp.tf

--- ReactApp.tf
+++ ReactApp.tf
@@ -10,9 +10,12 @@ resource "aws_instance" "React_app" {
     tags                         = { "Name" = "ReactApp(Terraform)" }
     key_name                     = "docker-putty"
 }
-output "public_ip" {
-  value = "${aws_instance.React_app.*.public_ip}"
-}
+provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> public_ips.txt"
+  }
+# output "public_ip" {
+#   value = "${aws_instance.React_app.*.public_ip}"
+# }
 
 resource "aws_security_group" "React_app" {
   name           = "ReactApp"
